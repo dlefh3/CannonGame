5b42275b19fe817f427eb2c943af7f0ba4f4f797
dlefh3 <dlefh3@mail.umsl.edu>
2015-04-20 13:09:20 -0500

    Initial commit

---
+++ app/src/main/java/com/fic/android/cannongame/Line.java
@@ -0,0 +1,11 @@
+package com.fic.android.cannongame;
+
+import android.graphics.Point;
+
+/**
+ * Created by User on 4/2/2015.
+ */
+public class Line {
+    public Point start = new Point(); // 0,0)
+    public Point end = new Point(); // (0,0)
+} // end class Line
